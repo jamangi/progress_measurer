e598db79668b21a796410916d600442219b9ec08
raspberry <jamangi.jmg@gmail.com>
2024-03-13 04:12:02 -0500

    update test_create_session

--- test_create_session.py
+++ test_create_session.py
@@ -1,6 +1,7 @@
 import json
 import prog_backend
 import os
+from datetime import datetime
 
 
 class EmptyUser:
@@ -17,6 +18,7 @@ def test_create_json():
         os.remove(filename)
 
     username_key = "username"
+    nickname_key = "nick"
     discord_id_key = "discord_id"
     hangout_title = "Sample Hangout"
 
@@ -31,33 +33,58 @@ def test_create_json():
         "subtask1": "Task 1",
         "subtask2": "Task 2",
         "subtask3": "Task 3",
-        "subtask4": "Task 4",
-        "subtask5": "Task 5"
+        "subtask4": "Task X",
+        "subtask5": None
     }
 
     data2 = {
-        "filename": filename,
+        # "filename": filename, <- filename not added to the json file by create
         "hangout_name": hangout_title,
         "duration": 60,
-        "maker": {username_key: "JimJam", discord_id_key: 1},
-        "participant2": {username_key: "Jack", discord_id_key: 2},
-        "participant3": {username_key: "Jill", discord_id_key: 3},
-        "participant4": {username_key: "Princess Peach", discord_id_key: 4},
-        "subtask1": "Task 1",
-        "subtask2": "Task 2",
-        "subtask3": "Task 3",
-        "subtask4": "Task 4",
-        "subtask5": "Task 5"
+        "maker": {username_key: "TestUser", discord_id_key: 1},  # username instead of nickname
+        "participants": [  # converts participants into a list and includes the maker
+            {nickname_key: "JimJam", discord_id_key: 1},
+            {nickname_key: "Jack", discord_id_key: 2},
+            {nickname_key: "Jill", discord_id_key: 3},
+            {nickname_key: "Princess Peach", discord_id_key: 4},
+        ],
+        "subtasks": [  # converts subtasks into a list
+            {'subtask': "Task 1", 'finished': False},
+            {'subtask': "Task 2", 'finished': False},
+            {'subtask': "Task 3", 'finished': False},
+            {'subtask': "Task X", 'finished': False},
+            # {'subtask': None, 'finished': False} # This task isn't included since it was None
+        ],
+        'start_time': int(datetime.now().timestamp()),  # create function adds this
+        'end_time': None  # create function adds this
     }
 
     #######################
     #######################
 
-    expected_data = [data2]
-
-    prog_backend.create_session(**data1) # requires keyword args
+    prog_backend.create_session(**data1)  # requires keyword args
 
     # check json
     with open(filename, mode='r', encoding='utf-8') as f:
         res = json.load(f)
-        assert isinstance(res, list) and res[0] == expected_data[0]
+        assert isinstance(res, list)
+        data = res[0]  # the entry
+        assert isinstance(data, dict) and data.get('maker')
+        assert data.get('duration') and data['duration'] == 60
+        assert data.get('maker') == data2['maker']
+
+        # to compare a list of dictionaries, I'll need to turn the dictionaries into frozen sets
+        set_expected = [frozenset(d) for d in data2.get('participants')]
+        set_actual = [frozenset(d) for d in data.get('participants')]
+        assert set(set_expected) == set(set_actual)
+
+        set_expected = [frozenset(d) for d in data2.get('subtasks')]
+        set_actual = [frozenset(d) for d in data.get('subtasks')]
+        assert set(set_expected) == set(set_actual)
+
+        start_time_expected = data2.get('start_time')
+        start_time_actual = data.get('start_time')
+        assert abs(start_time_expected - start_time_actual) < 5  # less than 5 unix time seconds off from test
+
+        # end_time is in the dictionary but set to None
+        assert 'end_time' in data and data.get('end_time') is None
