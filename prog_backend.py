655d6449bee2f75df30140ba55af467000e21e54
tetragrey <graizhon@gmail.com>
2024-03-13 14:38:34 +0100

    Fix create_session bug where duration was added in minutes

--- prog_backend.py
+++ prog_backend.py
@@ -6,21 +6,21 @@ from datetime import datetime
 
 
 def create_session(filename, hangout_name, duration, maker, subtask1, subtask2, subtask3, subtask4, subtask5,
-participant2=None, participant3=None, participant4=None):
+                   participant2=None, participant3=None, participant4=None):
     """Organizes the session into a dict and then adds that dict to the json file
     
-    :param filename: (str)
-    :param hangout_name: (str)
-    :param duration: (int)
-    :param maker: (user object)
-    :param subtask1: (str)
-    :param subtask2: (str)
-    :param subtask3: (str)
-    :param subtask4: (str)
-    :param subtask5: (str)
-    :param participant2: (user object) (optional)
-    :param participant3: (user object) (optional)
-    :param participant4: (user object) (optional)
+    :param filename: (str) the name of the file (which is in the cwd), plus .json
+    :param hangout_name: (str) the name given to the session
+    :param duration: (float) the number of minutes the session will last
+    :param maker: (user object) discord info for the person who made the session (and as such, participant1)
+    :param subtask1: (str) one of the subtasks that will be tackled in the session
+    :param subtask2: (str) one of the subtasks that will be tackled in the session
+    :param subtask3: (str) one of the subtasks that will be tackled in the session
+    :param subtask4: (str) one of the subtasks that will be tackled in the session
+    :param subtask5: (str) one of the subtasks that will be tackled in the session
+    :param participant2: (user object) (optional) Discord info for another participant in the session
+    :param participant3: (user object) (optional) Discord info for another participant in the session
+    :param participant4: (user object) (optional) Discord info for another participant in the session
     """
     # Make sure the database file is already there. If it's not, create it
     if not os.path.isfile(filename) or not os.path.isfile(filename):
@@ -48,7 +48,7 @@ participant2=None, participant3=None, participant4=None):
                               {'subtask': subtask4, 'finished': False},
                               {'subtask': subtask5, 'finished': False}],
                  'start_time': start_time,
-                 'end_time': start_time + duration
+                 'end_time': int(start_time + duration*60)
                  }
 
     # Read all entries in the json, then add a new one
